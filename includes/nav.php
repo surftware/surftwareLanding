481f6a4cece27b1b90b26d2e3f0d76f4ad0950ce
and <surftware@gmail.com>
2019-09-23 17:27:12 -0500

    avance includes

--- includes/nav.php
+++ includes/nav.php
@@ -1,119 +1,78 @@
 <body>
 
-<div class="wrap">
-	
-	<header class="header">
-		<div class="header__top">
-			<div class="center">
-				<ul class="info">
-					<li>
-						<a href="mailto:exampleemail@gmail.com">
-							<i class="fa fa-envelope"></i>
-							diaz_teran@hotmail.com
-						</a>						
-					</li>
-					<li>
-						<a href="tel:1-677-124-44227">
-							<i class="fa fa-phone"></i>
-							1-677-124-44227 
-						</a>	
-					</li>
-					<li>
-						<i class="fa fa-map-marker"></i>
-						437 S Olive St, Los Angeles
-					</li>
-				</ul>
-				<div class="soc clearfix">
-					<a href="#">
-						<i class="fa fa-twitter"></i>
-					</a>
-					<a href="#">
-						<i class="fa fa-linkedin"></i>
-					</a>
-					<a href="#">
-						<i class="fa fa-google-plus"></i>
-					</a>
-					<a href="#">
-						<i class="fa fa-facebook"></i>
-					</a>
-				</div>
-			</div>
-		</div>
-		<div class="header__bottom">
-			<div class="center pr">
-				<a href="#" class="logo">
-					<img src="images/header_logo.png" alt="">
-				</a>
-				<div class="nav clearfix">					
-					<ul>
+	<div class="wrap">
+
+		<header class="header">
+			<div class="header__top">
+				<div class="center">
+					<ul class="info">
 						<li>
-							<a href="#services">Services</a>
-						</li>
-						<li>
-							<a href="#pricing">Pricing</a>
-						</li>
-						<li>
-							<a href="#contacts">Contacts</a>
-						</li>
-						<li>
-							<a>All blocks</a>
 							<ul>
-								<li><a href="#about">About</a></li>
-								<li><a href="#services">Services</a></li>
-								<li><a href="#featured_product">Featured Product</a></li>
-								<li><a href="#benefits">Benefits</a></li>
-								<li><a href="#statistics">Statistics</a></li>
-								<li><a href="#counters">Counters</a></li>
-								<li><a href="#ads_bar">Ads bar</a></li>
-								<li><a href="#team">Team</a></li>
-								<li><a href="#pricing">Pricing</a></li> 
-								<li><a href="#sponsors">Sponsors</a></li>
-								<li><a href="#testimonials">Testimonials</a></li>
-								<li><a href="#projects">Projects</a></li>
-								<li><a href="#faq">F.A.Q.</a></li>
-								<li><a href="#contacts">Contacts</a></li>
-								<li><a href="#subscription">Subscription</a></li>
+								<li><a href="mailto:exampleemail@gmail.com"><i
+											class="fa fa-envelope"></i>exampleemail@gmail.com</a></li>
+								<li><a href="tel:1-677-124-44227"><i class="fa fa-phone"></i>1-677-124-44227</a></li>
+								<li><span><i class="fa fa-map-marker"></i>437 S Olive St, Los Angeles</span></li>
 							</ul>
 						</li>
 					</ul>
-					<div class="btn__wrap__center">
-						<a href="#" class="btn btn__medium btn__orange">FREE CONSULTATION</a>
+					<div class="soc clearfix">
+						<a href="#">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-linkedin"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-google-plus"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-facebook"></i>
+						</a>
 					</div>
 				</div>
-				<div class="menu_toggle">
-					<i></i>
-				</div>
-				<div class="phone_nav">					
-					<ul>
-						<li>
-							<a href="#services">Services</a>
-						</li>
-						<li>
-							<a href="#pricing">Pricing</a>
-						</li>
-						<li>
-							<a href="#contacts">Contacts</a>
-						</li>
-						<li>
-							<a class="sub">All blocks</a>
-							<ul>
-								<li><a href="#about">About</a></li>
-								<li><a href="#services">Services</a></li>
-								<li><a href="#featured_product">Featured Product</a></li>
-								<li><a href="#benefits">Benefits</a></li>
-								<li><a href="#statistics">Statistics</a></li>
-								<li><a href="#counters">Counters</a></li>
-								<li><a href="#ads_bar">Ads bar</a></li>
-								<li><a href="#team">Team</a></li>
-								<li><a href="#pricing">Pricing</a></li> 
-								<li><a href="#sponsors">Sponsors</a></li>
-								<li><a href="#testimonials">Testimonials</a></li>
-								<li><a href="#projects">Projects</a></li>
-								<li><a href="#faq">F.A.Q.</a></li>
-								<li><a href="#contacts">Contacts</a></li>
-								<li><a href="#subscription">Subscription</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div>
 			</div>
+			<div class="header__bottom">
+				<div class="center pr">
+					<a href="#" class="logo">
+						<img src="images/header_logo.png" alt="">
+					</a>
+					<div class="nav clearfix">
+						<ul>
+
+							<li>
+								<a href="index.php">Acerca de Surftware</a>
+							</li>
+							<li>
+								<a href="servicios.php">Servicios</a>
+							</li>
+							<li>
+								<a href="tienda.php">Tienda Online</a>
+							</li>
+							<li>
+								<a href="blog.php">Blog</a>
+							</li>
+
+
+						</ul>
+						<div class="btn__wrap__center">
+							<a href="#contacts" class="btn btn__medium btn__orange">Contacto</a>
+						</div>
+					</div>
+					<div class="menu_toggle">
+						<i></i>
+					</div>
+					<div class="phone_nav">
+						<ul>
+							<li>
+								<a href="#services">Services</a>
+							</li>
+							<li>
+								<a href="#pricing">Pricing</a>
+							</li>
+							<li>
+								<a href="#contacts">Contacts</a>
+							</li>
+
+						</ul>
+					</div>
+				</div>
---
+++ tienda.php
@@ -0,0 +1,14 @@
+<?php
+	 
+	define('BASE_PATH','/var/www/surftwarelanding.local/');
+	 
+	include (BASE_PATH."includes/head.php"); 
+	 
+	include (BASE_PATH."includes/nav.php");
+	  
+ ?>
+ <?php
+define('BASE_PATH','/var/www/surftwarelanding.local/');
+  include (BASE_PATH."includes/footer.php");
+  include (BASE_PATH."includes/js.php");
+?>
