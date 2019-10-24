96358323f993a30d9e15c91a5b2348ea711c27b8
and <surftware@gmail.com>
2019-10-24 14:51:44 -0500

    favicon

--- includes/head.php
+++ includes/head.php
@@ -4,12 +4,12 @@
 <head>
     <title>Surftware | Servicio de diseño web</title>
     <!--favicon -->
-    <link rel="apple-touch-icon" sizes="180x180"    href="favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
     <link rel="manifest" href="favicon/site.webmanifest">
-    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#12233b">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#212529">
+    <meta name="msapplication-TileColor" content="#212529">
     <meta name="theme-color" content="#ffffff">
 
     <!-- Required meta tags -->
