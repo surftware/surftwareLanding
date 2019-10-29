696fc88fa78e5aac4a5b74cea9e1404d574135d7
and <surftware@gmail.com>
2019-10-29 14:08:59 -0600

    adición de servicios

--- servicios.php
+++ servicios.php
@@ -33,8 +33,8 @@
 <div class="block services" id="services">
 		<div class="center">
 			<div class="block__title">
-				<span>Digital marketing services for your businesses</span>
-				Our Most Popular Services
+				<span>Servicio de TI para tus negocios</span>
+				Servicios Mas Populares
 			</div>
 
 			<div class="item__list">
@@ -43,7 +43,7 @@
 						<img src="images/services_icon_1.png" alt="">
 					</figure>
 					<div class="title">
-						Pay Per Click Management
+					Instalación de redes computadoras
 					</div>
 					<p>
 						Build a steady flow of leads or online sales with ROI based online advertising.
@@ -54,7 +54,7 @@
 						<img src="images/services_icon_2.png" alt="">
 					</figure>
 					<div class="title">
-						Content marketing
+					Instalación de servidores
 					</div>
 					<p>
 						We work together with our clients to come up with effective content strategies.
@@ -65,7 +65,7 @@
 						<img src="images/services_icon_3.png" alt="">
 					</figure>
 					<div class="title">
-						Adwords
+					Mantenimiento / Reparación equipo de cómputo
 					</div>
 					<p>
 						We diligently search for all keywords that are relevant to our client’s business.
@@ -76,7 +76,7 @@
 						<img src="images/services_icon_4.png" alt="">
 					</figure>
 					<div class="title">
-						Social Media Marketing
+					Administración / Recuperación / Respaldo de información
 					</div>
 					<p>
 						Grow the followers to increase brand awareness, sales and marketing potential.
@@ -87,7 +87,7 @@
 						<img src="images/services_icon_5.png" alt="">
 					</figure>
 					<div class="title">
-						Lead Generation
+					Soporte Remoto (Skype / Teamviewer )
 					</div>
 					<p>
 						Attract more visitors and deliver the message you want clients to hear.
@@ -98,7 +98,40 @@
 						<img src="images/services_icon_6.png" alt="">
 					</figure>
 					<div class="title">
-						Reporting & Analytics
+					Instalación de software
+					</div>
+					<p>
+						We carefully study Google analytics data and create comprehensive reports.
+					</p>
+				</div>
+				<div class="item wow flipInY" data-wow-delay="0.5s">
+					<figure>
+						<img src="images/services_icon_6.png" alt="">
+					</figure>
+					<div class="title">
+					Creación de sitios web
+					</div>
+					<p>
+						We carefully study Google analytics data and create comprehensive reports.
+					</p>
+				</div>
+				<div class="item wow flipInY" data-wow-delay="0.5s">
+					<figure>
+						<img src="images/services_icon_6.png" alt="">
+					</figure>
+					<div class="title">
+					Soporte de sitios web
+					</div>
+					<p>
+						We carefully study Google analytics data and create comprehensive reports.
+					</p>
+				</div>
+				<div class="item wow flipInY" data-wow-delay="0.5s">
+					<figure>
+						<img src="images/services_icon_6.png" alt="">
+					</figure>
+					<div class="title">
+					Venta de equipos de cómputo y accesorios
 					</div>
 					<p>
 						We carefully study Google analytics data and create comprehensive reports.
@@ -107,6 +140,261 @@
 			</div>
 		</div>
 	</div>
+	<div class="block pricing" id="pricing">
+		<div class="center">
+			<div class="block__title wow bounceIn">
+				Soporte / Limpieza / Windows
+			</div>
+
+			<div class="item__list">
+				<div class="item wow fadeInUp">
+					<div class="item__part__wrap">
+						<div class="item__part">
+							<div class="top">
+								Remoto
+							</div>
+							<div class="price">
+								<span> $200 </span>
+
+							</div>
+							<ul>
+								<li>Respuesta inmediata vía Team Viewer</li>
+								<li>Limpieza general del sistema Windows</li>
+								<li>Optimización</li>
+								<li>Instalación de programas (máx. 3)</li>
+								<li>Instalación antivirus</li>
+								<li>Consultoría, configuraciones, sistema y software</li>
+							</ul>
+						</div>
+						<div class="item__part">
+							<div class="btn__wrap__center">
+								<a href="servicios.php#contacts" class="btn btn__orange btn__large">Solicítalo
+									ahora!</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item wow fadeInUp" data-wow-delay="0.2s">
+					<div class="item__part__wrap">
+						<div class="item__part">
+							<div class="top">
+								Optimización Limpieza
+							</div>
+							<div class="price">
+								<span>$300</span>
+
+							</div>
+							<ul>
+								<li>Soporte personalizado</li>
+								<li>Limpieza general del sistema Windows</li>
+								<li>Optimización </li>
+								<li>Instalación de programas (máx. 3)</li>
+								<li>Instalación antivirus</li>
+								<li>Consultoría, configuraciones, sistema y software</li>
+							</ul>
+						</div>
+						<div class="item__part">
+							<div class="btn__wrap__center">
+								<a href="servicios.php#contacts" class="btn btn__orange btn__large">Solicítalo
+									ahora!</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item wow fadeInUp" data-wow-delay="0.4s">
+					<div class="item__part__wrap">
+						<div class="item__part">
+							<div class="top">
+								Formateo
+							</div>
+							<div class="price">
+								<span>$450</span>
+
+							</div>
+							<ul>
+								<li>Instalación de Windows</li>
+								<li>Optimización</li>
+								<li>Instalación de Programas</li>
+								<li>Antivirus</li>
+
+							</ul>
+						</div>
+						<div class="item__part">
+							<div class="btn__wrap__center">
+								<a href="servicios.php#contacts" class="btn btn__orange btn__large">Solicítalo
+									ahora!</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="block faq" id="faq">
+		<div class="center">
+			<div class="item__list  wow fadeInUp" data-wow-delay="0.2s">
+				<div class="item">
+					<div class="item__title">
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+							x="0px" y="0px" width="451.847px" height="451.846px" viewBox="0 0 451.847 451.846"
+							style="enable-background:new 0 0 451.847 451.846;" xml:space="preserve">
+							<g>
+								<path d="M248.292,106.406l194.281,194.29c12.365,12.359,12.365,32.391,0,44.744c-12.354,12.354-32.391,12.354-44.744,0
+									L225.923,173.529L54.018,345.44c-12.36,12.354-32.395,12.354-44.748,0c-12.359-12.354-12.359-32.391,0-44.75L203.554,106.4
+									c6.18-6.174,14.271-9.259,22.369-9.259C234.018,97.141,242.115,100.232,248.292,106.406z" />
+							</g>
+						</svg>
+						Why do I need a marketing plan?
+					</div>
+					<div class="item__descr">
+						<div class="article">
+							<div class="descr">
+								<p>
+									Is the science of adjusting a website’s code, content and structure to make it
+									visible on a search engine result page for particular keywords or combinations of
+									keywords.
+								</p>
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="item__title">
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+							x="0px" y="0px" width="451.847px" height="451.846px" viewBox="0 0 451.847 451.846"
+							style="enable-background:new 0 0 451.847 451.846;" xml:space="preserve">
+							<g>
+								<path d="M248.292,106.406l194.281,194.29c12.365,12.359,12.365,32.391,0,44.744c-12.354,12.354-32.391,12.354-44.744,0
+									L225.923,173.529L54.018,345.44c-12.36,12.354-32.395,12.354-44.748,0c-12.359-12.354-12.359-32.391,0-44.75L203.554,106.4
+									c6.18-6.174,14.271-9.259,22.369-9.259C234.018,97.141,242.115,100.232,248.292,106.406z" />
+							</g>
+						</svg>
+						Why do I need to market my business online?
+					</div>
+					<div class="item__descr">
+						<div class="article">
+							<div class="descr">
+								<p>
+									Is the science of adjusting a website’s code, content and structure to make it
+									visible on a search engine result page for particular keywords or combinations of
+									keywords.
+								</p>
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="item__title">
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+							x="0px" y="0px" width="451.847px" height="451.846px" viewBox="0 0 451.847 451.846"
+							style="enable-background:new 0 0 451.847 451.846;" xml:space="preserve">
+							<g>
+								<path d="M248.292,106.406l194.281,194.29c12.365,12.359,12.365,32.391,0,44.744c-12.354,12.354-32.391,12.354-44.744,0
+									L225.923,173.529L54.018,345.44c-12.36,12.354-32.395,12.354-44.748,0c-12.359-12.354-12.359-32.391,0-44.75L203.554,106.4
+									c6.18-6.174,14.271-9.259,22.369-9.259C234.018,97.141,242.115,100.232,248.292,106.406z" />
+							</g>
+						</svg>
+						How long does it take to see the results?
+					</div>
+					<div class="item__descr">
+						<div class="article">
+							<div class="descr">
+								<p>
+									Is the science of adjusting a website’s code, content and structure to make it
+									visible on a search engine result page for particular keywords or combinations of
+									keywords.
+								</p>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item__list  wow fadeInUp" data-wow-delay="0.2s">
+				<div class="item">
+					<div class="item__title">
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+							x="0px" y="0px" width="451.847px" height="451.846px" viewBox="0 0 451.847 451.846"
+							style="enable-background:new 0 0 451.847 451.846;" xml:space="preserve">
+							<g>
+								<path d="M248.292,106.406l194.281,194.29c12.365,12.359,12.365,32.391,0,44.744c-12.354,12.354-32.391,12.354-44.744,0
+									L225.923,173.529L54.018,345.44c-12.36,12.354-32.395,12.354-44.748,0c-12.359-12.354-12.359-32.391,0-44.75L203.554,106.4
+									c6.18-6.174,14.271-9.259,22.369-9.259C234.018,97.141,242.115,100.232,248.292,106.406z" />
+							</g>
+						</svg>
+						What is PPC?
+					</div>
+					<div class="item__descr">
+						<div class="article">
+							<div class="descr">
+								<p>
+									Is the science of adjusting a website’s code, content and structure to make it
+									visible on a search engine result page for particular keywords or combinations of
+									keywords.
+								</p>
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="item__title">
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+							x="0px" y="0px" width="451.847px" height="451.846px" viewBox="0 0 451.847 451.846"
+							style="enable-background:new 0 0 451.847 451.846;" xml:space="preserve">
+							<g>
+								<path d="M248.292,106.406l194.281,194.29c12.365,12.359,12.365,32.391,0,44.744c-12.354,12.354-32.391,12.354-44.744,0
+									L225.923,173.529L54.018,345.44c-12.36,12.354-32.395,12.354-44.748,0c-12.359-12.354-12.359-32.391,0-44.75L203.554,106.4
+									c6.18-6.174,14.271-9.259,22.369-9.259C234.018,97.141,242.115,100.232,248.292,106.406z" />
+							</g>
+						</svg>
+						What is SMM?
+					</div>
+					<div class="item__descr">
+						<div class="article">
+							<div class="descr">
+								<p>
+									Is the science of adjusting a website’s code, content and structure to make it
+									visible on a search engine result page for particular keywords or combinations of
+									keywords.
+								</p>
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="item__title">
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+							x="0px" y="0px" width="451.847px" height="451.846px" viewBox="0 0 451.847 451.846"
+							style="enable-background:new 0 0 451.847 451.846;" xml:space="preserve">
+							<g>
+								<path d="M248.292,106.406l194.281,194.29c12.365,12.359,12.365,32.391,0,44.744c-12.354,12.354-32.391,12.354-44.744,0
+									L225.923,173.529L54.018,345.44c-12.36,12.354-32.395,12.354-44.748,0c-12.359-12.354-12.359-32.391,0-44.75L203.554,106.4
+									c6.18-6.174,14.271-9.259,22.369-9.259C234.018,97.141,242.115,100.232,248.292,106.406z" />
+							</g>
+						</svg>
+						I’ve heard PPC is a waste of money. Should I use it?
+					</div>
+					<div class="item__descr">
+						<div class="article">
+							<div class="descr">
+								<p>
+									Is the science of adjusting a website’s code, content and structure to make it
+									visible on a search engine result page for particular keywords or combinations of
+									keywords.
+								</p>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 
 
 <div class="block contacts" id="contacts">
