31b5c59e4f5b7efa925c4dd68a5b3a5ca4af9da2
and <surftware@gmail.com>
2021-04-27 16:12:13 -0500

    avance tutoriales 2

--- tutoriales.php
+++ tutoriales.php
@@ -32,62 +32,66 @@
 	<div class="block about" id="about">
 		<div class="center">
 			<div class="about__wrap">
+
+				<figure class=" wow slideInRight">
+
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Z2L5J3G-sgA"
+						title="YouTube video player" frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen></iframe>
+
+				</figure>
+				
 				<div class="left_side  wow slideInLeft">
 					<div class="block__title">
 						<span>Tutoriales</span>
-						QGIS 3.10
-					</div>
+						QGIS 3.10</div>
 					<p>
 						Selección de un conjunto de inmuebles en QGIS 3.10
 					</p>
 					<div class="btn__wrap__left">
-						<a href="https://www.youtube.com/watch?v=Z2L5J3G-sgA&t=1s" target="_blank" class="btn btn__large btn__blue">Ver en YouTube</a>
+						<a href="https://www.youtube.com/watch?v=Z2L5J3G-sgA&t=1s" target="_blank"
+							class="btn btn__large btn__blue">Ver en YouTube</a>
 					</div>
 				</div>
-				<figure class=" wow slideInRight">
-                
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/Z2L5J3G-sgA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-				</figure>
 			</div>
 		</div>
 	</div>
+</div>
 
 <div class="">
 	<div class="block about" id="about">
 		<div class="center">
 			<div class="about__wrap">
-				<div class="right_side  wow slideInRight">
+				<div class="left_side  wow slideInLeft">
 					<div class="block__title">
-						<span>""</span>
-						Gestor de arranque rEFInd para instalar Windows en MacOs Sierra
+						<span>Tutoriales</span>
+						Instala Windows 10 en Mac
 					</div>
 					<p>
-						Instalación del gestor de arranque rEFInd en MacOs Sierra para instalar Windows
+						Uso de rEFInd para instalar Windows en una Mac
 					</p>
-					<div class="btn__wrap__rigth">
-						<a href="https://www.youtube.com/watch?v=Ilvf8nObIlA" target="_blank" class="btn btn__large btn__blue">Ver en YouTube</a>
+					<div class="btn__wrap__left">
+						<a href="https://www.youtube.com/watch?v=Ilvf8nObIlA" target="_blank"
+							class="btn btn__large btn__blue">Ver en YouTube</a>
 					</div>
 				</div>
-				<figure class=" wow slideInLeft">
-                
-                <iframe width="560" height="315" src="https://www.youtube.com/watch?v=Ilvf8nObIlA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<figure class=" wow slideInRight">
+
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Ilvf8nObIlA"
+						title="YouTube video player" frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen></iframe>
 
 				</figure>
 			</div>
 		</div>
 	</div>
+</div>
 
 
-
-
-
-
-
-
-
-
-    <?php
+<?php
 	
 	include ("includes/footer.php");
 	include ("includes/js.php");
