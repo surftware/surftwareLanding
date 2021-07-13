c2fbe2a75f0e3d9177e2db11512c902ea107e230
isaac <coronai-saac@hotmail.com>
2021-07-13 14:08:04 -0500

    portada

--- servicios.php
+++ servicios.php
@@ -28,7 +28,7 @@
 	<figure class="wow fadeInLeft">
 		<img  src="images/reparacion-computadoras1.jpg" alt="">
 	</figure>
-	<div class="textEncimado  wow slideInLeft">
+	<div class="textEncimado  wow slideInLeft posicion-texto">
 		<div class="text_1">
 			Surfware
 		</div>
