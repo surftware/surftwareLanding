83a1b5fbe2147233dfe90e825805baccbb9f44b0
and <surfwtare@gmail.com>
2021-07-15 03:57:10 -0500

    cambios js recaptcha bien

--- includes/js.php
+++ includes/js.php
@@ -1,6 +1,5 @@
 <!-- custom script -->
 <script src="js/main.js"></script>
-<script src="js/myscript.js"></script>
 
 <!-- bootstrap js-->
 <script src="bootstrap/js/bootstrap.min.js"></script>
@@ -11,22 +10,16 @@
 <!-- jquery -->
 <script src="js/jquery.min.js"></script>
 
+<!-- custom script -->
+<script src="js/myscript.js"></script>
+
 <!-- owl carousel -->
 <script src="js/owl.carousel.min.js"></script>
 
 <!-- wow -->
 <script src="js/wow.min.js"></script>
 
-<!-- alert -->
 <script src="js/sweetalert2.js"></script>
-
-<!-- reCaptcha -->
-<script src="https://www.google.com/recaptcha/api.js"></script>
-
-
-<!--ReCapcha V2 -->
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
 
 <script>
 	function myFunction() {
@@ -36,6 +29,10 @@
 		document.getElementById("demo").innerHTML = "Horizontally: " + x + "px<br>Vertically: " + y + "px";
 	}
 </script>
+
+<!--ReCapcha V2 -->
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
 
 <script>
 	function onSubmit(token) {
