384cffc304c140580f0f867dc73ca4ebdc681958
and <surfwtare@gmail.com>
2021-07-15 04:42:15 -0500

    recaptcha

--- includes/js.php
+++ includes/js.php
@@ -33,9 +33,3 @@
 <!--ReCapcha V2 -->
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-
-<script>
-	function onSubmit(token) {
-		document.getElementById("mailForm").submit();
-	}
-</script>
