4dd6a13e28e22b1fbc37d4c46a96e085f9c90926
isaac <coronai-saac@hotmail.com>
2021-07-19 14:46:39 -0500

    cambio color dots y  ajuste en redes Sociales

--- includes/js.php
+++ includes/js.php
@@ -30,6 +30,11 @@
 	}
 </script>
 
-<!--ReCapcha V2 -->
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<!--ReCapcha V3 -->
+<script src="https://www.google.com/recaptcha/api.js"></script>
+<script>
+   function onSubmit(token) {
+     document.getElementById("demo-form").submit();
+   }
+ </script>
 
