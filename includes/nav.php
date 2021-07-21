6da855c676a9a93dd7d9de582f36ff7a845f11c5
and <surfwtare@gmail.com>
2021-07-21 15:56:35 -0500

    revisión responsive acerca

--- includes/nav.php
+++ includes/nav.php
@@ -7,7 +7,8 @@
 					<ul class="info">
 						<li>
 							<a href="mailto:contacto@surftware.com.mx">
-								<i class="fa fa-envelope"></i><span class="text-white ml-2">contacto@surftware.com.mx</span>
+								<i class="fa fa-envelope"></i><span
+									class="text-white ml-2">contacto@surftware.com.mx</span>
 
 							</a>
 						</li>
@@ -53,11 +54,11 @@
 								<a href="servicios.php#">Servicios</a>
 							</li>
 							<li>
-								<a href="tutoriales.php">+ Tutoriales</a>
+								<a href="tutoriales.php">+Tutoriales</a>
 								<ul>
-									<li><a href="tutoriales.php#QGIS">Tutorial QGIS 3.10</a></li>
-									<li><a href="tutoriales.php#MAC">Instalación de W10 en Mac</a></li>
-									<li><a href="tutoriales.php#LED">Construye tu lámpara LED</a></li>
+									<li><a href="tutoriales.php#QGIS"><i class="fas fa-play-circle"></i>&nbsp;Tutorial QGIS 3.10</a></li>
+									<li><a href="tutoriales.php#MAC"><i class="fas fa-play-circle"></i>&nbsp;Instalación de W10 en Mac</a></li>
+									<li><a href="tutoriales.php#LED"><i class="fas fa-play-circle"></i>&nbsp;Construye tu lámpara LED</a></li>
 								</ul>
 							</li>
 						</ul>
@@ -79,13 +80,12 @@
 						<a href="servicios.php#">Servicios</a>
 					</li>
 					<li>
-						<a class="sub">Tutoriales</a>
+						<a class="sub">+Tutoriales</a>
 						<ul>
-							<li><a href="tutoriales.php#about">About</a></li>
-							<li><a href="tutoriales.php#services">Services</a></li>
-							<li><a href="tutoriales.php#featured_product">Featured Product</a></li>
-
-						</ul>
+									<li><a href="tutoriales.php#QGIS"><i class="fas fa-play-circle"></i>&nbsp;Tutorial QGIS 3.10</a></li>
+									<li><a href="tutoriales.php#MAC"><i class="fas fa-play-circle"></i>&nbsp;Instalación de W10 en Mac</a></li>
+									<li><a href="tutoriales.php#LED"><i class="fas fa-play-circle"></i>&nbsp;Construye tu lámpara LED</a></li>
+								</ul>
 					</li>
 				</ul>
 			</div>
