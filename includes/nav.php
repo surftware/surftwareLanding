7ca3611dc56efe41bab9e7c6bc3d2f96ffdaee05
isaac <coronai-saac@hotmail.com>
2021-07-26 13:17:56 -0500

    correcion

--- includes/nav.php
+++ includes/nav.php
@@ -7,13 +7,12 @@
 					<ul class="info">
 						<li>
 							<a href="mailto:contacto@surftware.com.mx">
-								<i class="fa fa-envelope"></i><span
-									class="text-white ml-2">contacto@surftware.com.mx</span>
+								<i class="fa fa-envelope"></i><span class="text-white ml-2">contacto@surftware.com.mx</span>
 
 							</a>
 						</li>
-						<li> 
-							<a href="https://wa.me/+525511894621?text=Hola%20solicito%20ayuda%20desde%20surftware.com.mx%20"
+						<li>
+							<a href="https://api.whatsapp.com/send?phone=+525511894621&text=Hola, te contacto desde la página web de Surftware"
 								target="_blank">
 								<i class="fa fa-phone"></i><span class="text-white ml-2">5511894621</span>
 
@@ -21,7 +20,7 @@
 						</li>
 						<li>
 							<a href="https://g.page/surftware?share" target="_blank">
-								<i class="fa fa-map-marker"></i><span class="text-white ml-2">Norte 84-A 6515 San Pedro el
+								<i class="fa fa-map-marker"></i><span class="text-white ml-2">Nte. 84-A 6515 Sn Pedro el
 									Chico</span>
 							</a>
 						</li>
@@ -43,27 +42,27 @@
 			<div class="header__bottom">
 				<div class="center">
 					<a href="index.php" class="logo">
-						<img class="logoSurftware" src="images/creacion-paginas-web.webp" alt="">
+						<img class="logoSurftware" src="images/creacion-paginas-web.png" alt="">
 					</a>
 					<div class="nav clearfix">
 						<ul>
-							<li class="active">
+							<li>
 								<a href="index.php#about">Acerca de</a>
 							</li>
 							<li>
 								<a href="servicios.php#">Servicios</a>
 							</li>
 							<li>
-								<a href="tutoriales.php">Tutoriales&nbsp;<i class="fas fa-sort-down"></i></a>
+								<a>+ Tutoriales</a>
 								<ul>
-									<li><a href="tutoriales.php#QGIS"><i class="fas fa-play-circle"></i>&nbsp;Tutorial QGIS 3.10</a></li>
-									<li><a href="tutoriales.php#MAC"><i class="fas fa-play-circle"></i>&nbsp;Instalación de W10 en Mac</a></li>
-									<li><a href="tutoriales.php#LED"><i class="fas fa-play-circle"></i>&nbsp;Construye tu lámpara LED</a></li>
+									<li><a href="tutoriales.php#QGIS">Tutorial QGIS 3.10</a></li>
+									<li><a href="tutoriales.php#MAC">Instalación de W10 en Mac</a></li>
+									<li><a href="tutoriales.php#LED">Construye tu lámpara LED</a></li>
 								</ul>
 							</li>
 						</ul>
 						<div class="btn__wrap__center">
-							<a href="servicios.php#contacts" class="btn btn__medium btn__blue">Cont&aacute;ctanos!</a>
+							<a href="servicios.php#contacts" class="btn btn__medium btn__blue">Contáctanos!</a>
 						</div>
 					</div>
 					<div class="menu_toggle">
@@ -73,19 +72,20 @@
 			</div>
 			<div class="phone_nav">
 				<ul>
-					<li">
+					<li>
 						<a href="index.php#about">Acerca de</a>
 					</li>
 					<li>
 						<a href="servicios.php#">Servicios</a>
 					</li>
 					<li>
-						<a class="sub">Tutoriales&nbsp;<i class="fas fa-sort-down"></i></a>
+						<a class="sub">Tutoriales</a>
 						<ul>
-									<li><a href="tutoriales.php#QGIS"><i class="fas fa-play-circle"></i>&nbsp;Tutorial QGIS 3.10</a></li>
-									<li><a href="tutoriales.php#MAC"><i class="fas fa-play-circle"></i>&nbsp;Instalaci&oacute;n de W10 en Mac</a></li>
-									<li><a href="tutoriales.php#LED"><i class="fas fa-play-circle"></i>&nbsp;Construye tu l&aacute;mpara LED</a></li>
-								</ul>
+							<li><a href="tutoriales.php#about">About</a></li>
+							<li><a href="tutoriales.php#services">Services</a></li>
+							<li><a href="tutoriales.php#featured_product">Featured Product</a></li>
+
+						</ul>
 					</li>
 				</ul>
 			</div>
