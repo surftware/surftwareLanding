6ee5a5774feb07956f95e11b214bb8cdacb16073
RMG-hub2209 <rafael.madrigal@ciencias.unam.mx>
2021-10-12 13:30:24 -0500

    logo-link ecos

--- index.php
+++ index.php
@@ -92,6 +92,13 @@
 					</a>
 				</div>
 
+				<div  class="mt-4 pt-5">
+					<a href="https:/surftware.com.mx/servicios.php#contacts" target="_blank"><img
+							src="images/clientes/cliente-creacion-sitio-web-2.webp" alt="">
+						<p>Constructora Aldazy</p>
+					</a>
+				</div>
+
 				<div>
 					<a href="https://www.speaklikeamexican.com.mx/" target="_blank"><img
 							src="images/clientes/Logo_002.webp" alt="">
@@ -106,10 +113,11 @@
 					</a>
 				</div>
 
-				<div  class="mt-4 pt-5">
-					<a href="https:/surftware.com.mx/servicios.php#contacts" target="_blank"><img
-							src="images/clientes/cliente-creacion-sitio-web-2.webp" alt="">
-						<p>Constructora Aldazy</p>
+
+				<div>
+					<a href="https://ecosdemexico.com.mx/" target="_blank"><img
+							src="images/clientes/cliente-creacion-sitio-web-7.webp" alt="">
+						<p class="mt-3">Ecos de M&eacute;xico</p>
 					</a>
 				</div>
 
